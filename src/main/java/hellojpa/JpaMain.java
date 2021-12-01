d4b5f00f486c5261c38ef0912e85aceec2176f53
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-12-01 23:24:49 +0900

    [test] 1차 캐시, 동일성 보장 확인

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,15 +4,90 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.transaction.Transaction;
 
 public class JpaMain {
+
+    private static  EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa_basic_db");
+
+
     public static void main(String[] args) {
-        JpaMain jpaMain = new JpaMain();
-        jpaMain.findMember();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setName("hello");
+
+        entityManager.persist(member);
+
+        for(int i=0; i<10; i++) {
+            Member findMember = entityManager.find(Member.class, 1L);
+            System.out.println(findMember);
+        }
+
+        transaction.commit();
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+
+    /**
+     * 동일성 보장
+     * 1차 캐시에 존재하는 엔티티는 몇십, 몇백번을 읽어도 동일한 엔티티
+     */
+    void sameRead() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setName("hello");
+
+        entityManager.persist(member);
+
+        for(int i=0; i<10; i++) {
+            Member findMember = entityManager.find(Member.class, 1L);
+            System.out.println(findMember);
+        }
+
+        transaction.commit();
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+
+    /**
+     * 1차 캐시에서 읽어오기
+     * SELECT 쿼리가 X
+     */
+    void readCache() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setName("hello");
+
+        entityManager.persist(member);
+        Member findMember = entityManager.find(Member.class, 1L);
+
+        System.out.println("id : " + findMember.getId());
+        System.out.println("name : " + findMember.getName());
+
+        transaction.commit();
+
+        entityManager.close();
+        entityManagerFactory.close();
     }
 
     void updateMember() {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa_basic_db");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         EntityTransaction transaction = entityManager.getTransaction();
@@ -27,12 +102,9 @@ public class JpaMain {
         } finally {
             entityManager.close();
         }
-
-        entityManagerFactory.close();
     }
 
     void deleteMember() {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa_basic_db");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         EntityTransaction transaction = entityManager.getTransaction();
@@ -47,31 +119,26 @@ public class JpaMain {
         } finally {
             entityManager.close();
         }
-
-        entityManagerFactory.close();
     }
 
     void findMember() {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa_basic_db");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
 
         try {
-            Member findMember = entityManager.find(Member.class, 1L);
+            Member findMember = entityManager.find(Member.class, "hello");
             transaction.commit();
+            System.out.println(findMember);
         } catch (Exception e) {
             transaction.rollback();
         } finally {
             entityManager.close();
         }
-
-        entityManagerFactory.close();
     }
 
     void saveMember() {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa_basic_db");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         EntityTransaction transaction = entityManager.getTransaction();
@@ -90,6 +157,5 @@ public class JpaMain {
             entityManager.close();
         }
 
-        entityManagerFactory.close();
     }
 }
